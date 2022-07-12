2cb0b04b6d1c75cec8913bd79ea65475adf3499a
Gia Bao Nguyen Hoang <giabao.nguyenhoang1001@gmail.com>
2022-07-12 19:16:04 +0700

    sending a change-password request from frontend (fix needed)

--- components/profile/profile-form.js
+++ components/profile/profile-form.js
@@ -1,15 +1,31 @@
-import classes from './profile-form.module.css';
+import classes from "./profile-form.module.css";
+import { useRef } from "react";
 
-function ProfileForm() {
+function ProfileForm(props) {
+  const oldInputRef = useRef();
+  const newInputRef = useRef();
+
+  async function submitHandler(e) {
+    e.preventDefault();
+
+    const enteredNewPassword = newInputRef.current.value;
+    const enteredOldPassword = oldInputRef.current.value;
+
+    props.onChangePassword({
+      oldPassword: enteredOldPassword,
+      newPassword: enteredNewPassword,
+    });
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input type="password" id="new-password" ref={newInputRef} />
       </div>
       <div className={classes.control}>
-        <label htmlFor='old-password'>Old Password</label>
-        <input type='password' id='old-password' />
+        <label htmlFor="old-password">Old Password</label>
+        <input type="password" id="old-password" ref={oldInputRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
--- pages/api/user/change-password.js
+++ pages/api/user/change-password.js
@@ -1,4 +1,7 @@
 import { getSession } from "next-auth/client";
+import { ConnectToDatabase } from "../../../lib/db";
+import { verifyPassword, hashPassword } from "../../../lib/auth";
+import { hash } from "bcryptjs";
 
 async function handler(req, res) {
   if (req.method !== "PATCH") {
@@ -11,6 +14,50 @@ async function handler(req, res) {
     res.status(401).json({ message: "Not authenticated!" });
     return;
   }
+
+  const userEmail = session.user.email;
+  const oldPassword = req.body.oldPassword;
+  const newPassword = req.body.newPassword;
+
+  const client = await ConnectToDatabase();
+
+  const userCollection = client.db("Authentication").collection("users");
+
+  const user = userCollection.findOne({ email: userEmail });
+
+  if (!user) {
+    res.status(404).json({ message: "User not found" });
+    client.close();
+    return;
+  }
+
+  const currentPassword = user.password;
+
+  const passwordAreEqual = await verifyPassword(oldPassword, currentPassword);
+
+  if (!passwordAreEqual) {
+    res.status(403).json({ message: "Invalid password" });
+    client.close();
+    return;
+  }
+
+  // hash the new password
+  const hashedPassword = hashPassword(newPassword);
+
+  // update the new password in the database
+  const result = await userCollection.updateOne(
+    {
+      email: userEmail,
+    },
+    {
+      $set: {
+        password: hashedPassword,
+      },
+    }
+  );
+
+  client.close();
+  res.status(200).json({ message: " Password updated successfully!" });
 }
 
 export default handler;
